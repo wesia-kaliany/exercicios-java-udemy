1065b3d94df49ca7266ab5c3e54fcc6c7af9f7f1
Wesia Kaliany <wesiakalianyl@gmail.com>
2026-08-25 15:13:00 -0300

    Projeto simples, apenas para relembrar do que foi visto na aula de saida de dados! :)

--- src/main/java/saida1/saidadedados1.java
+++ src/main/java/saida1/saidadedados1.java
@@ -10,4 +10,22 @@ package saida1;
  */
 public class saidadedados1 {
     
+  public static void main(String[] args) {
+        
+        String nome = "Joao Matheus"; 
+        int idade = 55;
+        double credito = 5.000;
+        String nome1 = "Carla Maria";
+        int idade1 = 49;
+        double renda = 6.000;
+      
+   
+        
+        System.out.printf("%s tem %d anos e possui %.3f de credito a receber no banco", nome, idade, credito);      
+        System.out.printf("\n%s tem %d anos e possui %.3f de renda salarial", nome1, idade1, renda);
+        
+    }
+  
+   
+   
 }
